5916bcbd5a68744702cdd52a5990cb816777918a
maksim <maximthebest@outlook.com>
2021-01-21 16:03:11 +0200

    User page and footer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     user_ids = current_user.friends.ids.append(current_user.id)
-    @posts = Post.where(author_id: user_ids)
+    @posts = Post.where(author_id: user_ids).order(created_at: :desc)
   end
 
   def new
--- app/javascript/bulma.js
+++ app/javascript/bulma.js
@@ -1,7 +1,15 @@
 document.addEventListener("turbolinks:load", function() {
+    // Close notifications
+    (document.querySelectorAll(".notification .delete") || []).forEach(($delete) => {
+        var $notification = $delete.parentNode;
+        $delete.addEventListener('click', () => {
+            $notification.parentNode.removeChild($notification);
+        });
+    });
+
+    // Toggle navbar on mobile
     burgerIcon = document.querySelector("#burger");
     navbarMenu = document.querySelector("#nav-links");
-
     burgerIcon.addEventListener("click", () => {
         navbarMenu.classList.toggle("is-active");
     });
